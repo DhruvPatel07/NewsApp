32faae1cbcc2a05d0921e0352732c631ec8a35e2
DHRUV PATEL <190630116025@mbit.edu.in>
2022-02-20 16:30:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <LoadingBar height={2} color="#f11946" progress={progress} />
+        <Switch>
+          <Route exact path="/">
+            <News
+              setProgress={setProgress}
+              key="general"
+              pageSize={6}
+              country="in"
+              category="General"
+            />
+          </Route>
+
+          <Route exact path="/business">
+            <News
+              setProgress={setProgress}
+              key="business"
+              pageSize={6}
+              country="in"
+              category="Business"
+            />
+          </Route>
+
+          <Route exact path="/entertainment">
+            <News
+              setProgress={setProgress}
+              key="entertainment"
+              pageSize={6}
+              country="in"
+              category="Entertainment"
+            />
+          </Route>
+
+          <Route exact path="/sports">
+            <News
+              setProgress={setProgress}
+              key="sports"
+              pageSize={6}
+              country="in"
+              category="Sports"
+            />
+          </Route>
+
+          <Route exact path="/technology">
+            <News
+              setProgress={setProgress}
+              key="technology"
+              pageSize={6}
+              country="in"
+              category="Technology"
+            />
+          </Route>
+
+          <Route exact path="/science">
+            <News
+              setProgress={setProgress}
+              key="science"
+              pageSize={6}
+              country="in"
+              category="Science"
+            />
+          </Route>
+
+          <Route exact path="/health">
+            <News
+              setProgress={setProgress}
+              key="health"
+              pageSize={6}
+              country="in"
+              category="Health"
+            />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
